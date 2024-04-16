951e576d0c1d642dc3e73479c8a1699f1104cc82
Ragavan0701 <116545605+Ragavan0701@users.noreply.github.com>
2024-04-16 10:39:27 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,13 +9,14 @@ import Backup from "../images/images.jpg"
     <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-3">
         <Link to={`/movie/${id}`}>
             <img className="rounded-t-lg" src={image} alt="" />
-        </Link>
+        
         <div className="p-5">
             <Link to={`/movie/${id}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
             </Link>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
         </div>
+  </Link>
     </div>
   )
 }
