ee5c1170af303a65e6f17fec42327e9042a0eacf
Ragavan0701 <116545605+Ragavan0701@users.noreply.github.com>
2024-04-16 10:41:06 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,8 +13,9 @@ import Backup from "../images/images.jpg"
         <div className="p-5">
             <Link to={`/movie/${id}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
             </Link>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+            
         </div>
   
     </div>
